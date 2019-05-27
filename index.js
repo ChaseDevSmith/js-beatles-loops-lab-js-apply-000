dd744ad5d8f2478587e4347fb5741598617cc84d
Chase Deveraux Smith <chases832@gmail.com>
2019-05-27 08:08:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){ 
+  var arr =[]
+  
+  for (let i = 0, l = musicians.length; i<l; i++){
+    arr.push(`${musicians[i]} plays ${instruments[i]}`)
+    
+  }
+  return arr 
+
+}
+  
+function johnLennonFacts(facts) {
+  const shoutedFacts = []
+  
+  let i = 0
+  while (i < facts.length) {
+    shoutedFacts.push(`${facts[i]}!!!`)
+    i++
+  }
+  return shoutedFacts
+}
